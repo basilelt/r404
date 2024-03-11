a7e9dc59567921e121a6152f68bab8aca6cb829b
Basile <lethiecb@protonmail.com>
2024-03-11 21:35:41 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,1 +1,12 @@
-test
+# Import the script you want to test
+. './your-script.ps1'
+
+# Define the test suite
+Describe "Test Suite" {
+    # Define a test case
+    It "Test Case 1" {
+        # Call a function from your script and check the output
+        $result = Your-Function -Param1 'value1' -Param2 'value2'
+        $result | Should -Be 'expected output'
+    }
+}
